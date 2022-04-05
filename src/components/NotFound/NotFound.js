df326e7b4f403d0487a2c6d4141d7050d8373a32
zillurahman10 <raihan.brac2019@gmail.com>
2022-04-05 22:38:29 +0600

    again last commit for mistake

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -8,7 +8,7 @@ const NotFound = () => {
                 <h2 className='d-flex justify-content-center'>Page Not Found!!</h2>
                 <img className='rounded-3' src="https://aioseo.com/wp-content/uploads/2021/04/how-to-find-and-fix-404-errors-in-wordpress.png" alt="" />
                 <div className='d-flex justify-content-center'>
-                    <Link className='btn btn-primary mt-3' to="/">Back ot home page</Link>
+                    <Link className='btn btn-primary mt-3' to="/">Back to home page</Link>
                 </div>
             </div>
         </div>
